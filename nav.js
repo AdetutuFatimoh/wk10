02bacd951acd43d432c90107e8170f3bba8cfaf5
AdetutuFatimoh <adetutufatimoh@gmail.com>
2023-06-27 08:30:31 -0700

    feat:add

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ const hmenu = document.getElementById('hmenu')
 const cancel = document.getElementById('cancel')
 
 hmenu.addEventListener('click', function(){
-    menu.classList.toggle = ('hidden')
+    menu.classList.toggle('hidden')
     hmenu.style.display = 'none'
     cancel.style.display = 'block'
      // menu.style.display = 'none'
